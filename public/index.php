2c27324f43ac6b5bc53d43a5b4f3ca9e926bb38b
Emil Virkki <emil.virkki@gmail.com>
2018-11-28 00:29:17 +0200

    Use .env

--- public/index.php
+++ public/index.php
@@ -3,12 +3,16 @@
 use App\Kernel;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Dotenv\Dotenv;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 // auto-load annotations
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
-$kernel = new Kernel('dev', true);
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
+
+$kernel = new Kernel(getenv('APP_ENV'), getenv('APP_DEBUG'));
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
